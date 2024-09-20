a05d89adb5fec50359a6ae68bc990e68f41684d5
Wagner Meira <143838799+wagnermeira@users.noreply.github.com>
2024-09-19 21:51:47 -0300

    Titulos

--- Exercícios/ex002_RespondendoUsuario.py
+++ Exercícios/ex002_RespondendoUsuario.py
@@ -1,2 +1,3 @@
+#Crie um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas
 nome = input('Digite seu nome: ')
 print('É um prazer te conhecer, {}!'.format(nome))
--- Exercícios/ex003_Somando2Numeros.py
+++ Exercícios/ex003_Somando2Numeros.py
@@ -1,3 +1,4 @@
+# Crie um programa que leia dois números e realize a soma entre eles
 n1 = int(input('Digite um valor: '))
 n2 = int(input('Digite outro valor: '))
 soma = n1 + n2
